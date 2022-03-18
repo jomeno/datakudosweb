254ff420262f94437f89db30612eab7b3f4af9fa
Jomeno Vona <jomeno.vona@gmail.com>
2022-03-18 09:55:59 +0400

    Completed metadata parser

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-//import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-//import { FieldTag, RelatedDatasetCard } from './Home';
 
 function App() {
-
 
   const [datasets, setDatasets] = useState<dataset[]>([])
   const [selectedDataset, setSelectedDataset] = useState<dataset>()
   const [relatedDatasets, setRelatedDatasets] = useState<dataset[]>([])
+  const [relatedFields, setRelatedFields] = useState<field[]>([])
   const [reportQuery, setReportQuery] = useState<string>()
   const [downloadRequested, setDownloadRequested] = useState<boolean>(false)
 
   const getRelatedDatasets =(dataset:dataset)=>{
     if(!dataset || !dataset.name) return [];
+
     console.log('Determine related datasets...')
     let datasetId = dataset.name + 'Id'
-    // Strip white spaces
-    datasetId = datasetId.replace(/ /g, '')
-    //console.log('The relationship is by', datasetId)
+
     // Get all datasets with this relationship in their fields
     let relatedDatasets: dataset[] = []
     for(let i = 0; i < datasets.length; i++){
       let fields = datasets[i].fields
-      fields.map(f=>{
-        let fieldLast2Characters = f.name.substring(f.name.length - 2)
+      fields.map(field=>{
+        let fieldLast2Characters = field.name.substring(field.name.length - 2)
         if(fieldLast2Characters === 'Id') {
           console.log('Found one or more related datasets')
 
@@ -45,16 +42,16 @@ function App() {
               let relationCommaFields = relatedDataset.fields.map(f=>f.name).join(',') // Medical Insurance
               let relatedDatasetId = relatedDataset.name + 'Id'
               
-                if(datasetCommaFields.includes(relatedDatasetId) || relationCommaFields.includes(datasetId)){
-                  relatedDatasets.push(relatedDataset)
-                  //console.log(dataset.name + ' has ' + relatedDatasetId + ' or ' + relatedDataset.name + ' has ' + datasetId)
-                }else{
-                  //console.log(dataset.name + ' does not have ' + relatedDatasetId + ' and ' + relatedDataset.name + ' does not have ' + datasetId)
-                }
+              if(datasetCommaFields.includes(relatedDatasetId) || relationCommaFields.includes(datasetId)){
+                relatedDatasets.push(relatedDataset)
+                //console.log(dataset.name + ' has ' + relatedDatasetId + ' or ' + relatedDataset.name + ' has ' + datasetId)
+              }else{
+                //console.log(dataset.name + ' does not have ' + relatedDatasetId + ' and ' + relatedDataset.name + ' does not have ' + datasetId)
+              }
               }
           }            
         }
-        return f        
+        return field        
       })
     }
     
@@ -63,16 +60,41 @@ function App() {
     return relatedDatasets
   }
 
-  const getResultQuery = (selectedDataset:dataset | undefined) => {
+  const getRelatedFields = (relatedDatasets:dataset[]): field[] => {
+
+    // Extract participating related fields
+
+    let relatedFields:field[] = []
+    relatedDatasets.forEach((d, datasetIndex) => d.fields.forEach((f, fieldIndex)=> {
+      f.datasetIndex = datasetIndex
+      if(fieldIndex > 0 && f.isSelected) relatedFields.push(f)
+    }))
+
+    return relatedFields
+  }
+
+  const getResultQuery = (selectedDataset:dataset | undefined, relatedFields:field[]) => {
     if(!selectedDataset) return
 
-    let baseEntity = selectedDataset.name
+    let baseDatasetName = selectedDataset.name
+    let baseQuery = 'SELECT * FROM ' + baseDatasetName + ' d0 '
 
-    let query = 'SELECT * FROM ' + baseEntity + ' E1 LEFT JOIN Compensation ON Employee.Id = Compensation.EmployeeId'
-    
-    console.log('Automatically generated SQL query with join...')
-    console.log(query)
-    setReportQuery(query)
+    let joinQuery = ''
+    for(let i = 0; i < relatedDatasets.length; i++){
+
+      let datasetName = relatedDatasets[i].name
+      let datasetAlias = 'd' + (i + 1)
+
+      // Only add a left join if there is atleast 1 field from the dataset
+      let fieldFromDataset = relatedFields.filter(f=>f.datasetIndex === i)[0]
+      if(fieldFromDataset){
+        joinQuery += 'LEFT JOIN ' + datasetName + ' ' + datasetAlias + ' ON d0.Id = ' + datasetAlias + '.' + baseDatasetName + 'Id '
+      }
+    }
+
+    let query = baseQuery + joinQuery
+
+    return query.trim()
   }
 
   const handleDownloadClick = ()=>{
@@ -80,14 +102,16 @@ function App() {
     console.log('User clicked Download.')    
     if(relatedDatasets.length > 0){
 
-      getResultQuery(selectedDataset)
+      console.log('Generating SQL query...')
+      let query = getResultQuery(selectedDataset, relatedFields)
+
+      console.log(query)
+      setReportQuery(query)
       setDownloadRequested(true)
       
     }else{
       console.log("But we're not interested in this boring scenario yet. Please select Employee dataset")
     }   
-    
-    //alert('Download in progress...')
   }
 
   const handleFieldClick = (relatedDatasetIndex:number, fieldIndex:number)=>{
@@ -96,7 +120,13 @@ function App() {
       console.log('User selected Field', relatedDatasets[relatedDatasetIndex].fields[fieldIndex])
       let isSelected = relatedDatasets[relatedDatasetIndex].fields[fieldIndex].isSelected
       relatedDatasets[relatedDatasetIndex].fields[fieldIndex].isSelected = !isSelected
+
+      let relatedFields = getRelatedFields(relatedDatasets)
+
       setRelatedDatasets([...relatedDatasets])
+      setRelatedFields(relatedFields)
+      let query = getResultQuery(selectedDataset, relatedFields)
+      setReportQuery(query)
     }
 
   }
@@ -106,11 +136,13 @@ function App() {
 
     // Get related datasets
     let relatedDatasets = getRelatedDatasets(dataset);
+    
+    setRelatedFields([...relatedFields])
 
     // Update state
     setRelatedDatasets([...relatedDatasets])
     setSelectedDataset(dataset)
-    getResultQuery(dataset)
+    setReportQuery('')
   }
 
     useEffect(()=>{
@@ -125,12 +157,6 @@ function App() {
       }).then(datasets=>{
         console.log('Metadata fetched from API, maybe powered by a document db like mongodb.', datasets)
         setDatasets(datasets)
-        
-        // if(datasets && datasets[0]) {
-        //   for(let i = 0; i < relatedDatasets.length; i++){
-        //     relatedDatasets[i].selectedFieldIndex = 0
-        //   }
-        // }
       })
 
     }, [])
@@ -151,7 +177,7 @@ function App() {
                 datasets.map((item, index)=>{
                   return (<Link to="#" onClick={()=>handleSelectDataset(item)} className={selectedDataset?.name === item?.name ? 'list-group-item list-group-item-dark active' : 'list-group-item list-group-item-action'} key={index}>
                     <div className="d-flex w-100 justify-content-between">
-                      <h5 className="mb-1">{item.name}</h5>
+                      <h5 className="mb-1">{item.friendlyName}</h5>
                       <small className='d-none'>3 fields</small>
                     </div>
                     <p className="mb-1 text-left">{
@@ -174,9 +200,9 @@ function App() {
           <div className="col-md-8 p-4">
             <div className="mb-5">
             <div className="p-5 mb-4 bg-light rounded-3">
-              <div className="container-fluid py-5">
-                <h1 className="display-5 fw-bold">{selectedDataset?.name} {!selectedDataset?'Reports':'reports'}</h1>
-                <p className="col-md-8 fs-4">What {selectedDataset?.name} information do you need?</p>
+              <div className="container-fluid py-3">
+                <h1 className="display-5 fw-bold">{selectedDataset?.friendlyName} {!selectedDataset?'Reports':'reports'}</h1>
+                <p className="col-md-8 fs-4">What {selectedDataset?.friendlyName} information do you need?</p>
 
                 {
                   selectedDataset? <p className="fs-4">
@@ -184,27 +210,38 @@ function App() {
                 </p>: <></>
                 }
                 
-                <p className="d-none">The related datasets below have been determined automatically by parsing metadata only, sparing the user the agony of manually creating and maintainig joins for their reports.</p>
-                {selectedDataset ? <button className="btn btn-primary btn-lg" onClick={()=> handleDownloadClick()} type="button">Download</button> : <></>}
-                {downloadRequested && <p className="pt-5"><code className="fs-5">{reportQuery}</code></p>}
+                <p className="pt-4"><code className="fs-5">{reportQuery}</code></p>
+                {selectedDataset ? <button className="btn btn-primary btn-lg mb-3" onClick={()=> handleDownloadClick()} type="button">Download</button> : <></>}
+                
+                {
+                  downloadRequested && 
+                  <>
+                    <p><small>Okay, sending your request off to a backend service, will keep you posted.</small></p>
+                  </>
+                }                
+                
               </div>
             </div>
             </div>
             {
-              relatedDatasets.length > 0 ? 
+              relatedDatasets.length > 0 && 
                 <>
                   <h5>
-                    Related details
-                </h5>
-                <p>
-                  Enrich your report by adding related information easily.
-                </p>
-                </>                
-                :
-                <></>
+                      {
+                        relatedFields.length === 0 ? <>Related details</>
+                        :
+                        <>
+                          {
+                            relatedFields.length === 1 ? '1 Related detail' : relatedFields.length + ' Related details'
+                          }                      
+                        </>
+                      }
+                  </h5>
+                  <p>
+                    Enrich your report by adding related information easily.
+                  </p>
+                </>
             }
-            
-            
 
             {
               // relatedDatasets?.map((relation, index)=>{
@@ -226,7 +263,7 @@ function App() {
                     return(
                       <div className="card" key={relationIndex}>
                         <div className="card-body">
-                          <h5 className="card-title">{relation.name}</h5>
+                          <h5 className="card-title">{relation.friendlyName}</h5>
                           <p className="card-text">{relationIndex === 0 ? 'Toggle to add or remove' : 'Maybe some basic user instruction here.'}</p>                    
                           
                           <div className="mb-3">
@@ -235,7 +272,7 @@ function App() {
                                 if(fieldIndex === 0) return null;
                                 return (<Link to="#" onClick={()=> handleFieldClick(relationIndex, fieldIndex)} key={fieldIndex}>
                                   <span className={field.isSelected ? 'badge rounded-pill bg-light text-dark border border-secondary me-1' : 'badge rounded-pill bg-light text-dark me-1'}>
-                                    {field.name}
+                                    {field.friendlyName}
                                   </span></Link>)
 
                               })
--- src/model.d.ts
+++ src/model.d.ts
@@ -1,11 +1,14 @@
 type dataset = {
     name:string;
+    friendlyName:string;
     fields:field[];
     //relations:dataset[];
   }
   
   type field = {
     name:string;
+    friendlyName:string;
     type:string;
     isSelected:boolean;
+    datasetIndex:number;
   }
